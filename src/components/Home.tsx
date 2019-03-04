f1361c5604bf7caff28a3bd2bd14dfa0786521b1
dvaJi <francisco_p@outlook.com>
2019-03-03 21:25:17 -0300

    feat: add new theme

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 import ShowFilesPage from '../containers/ShowFilesPage';
 
-import { fileTemplate } from '../utils/helpers';
-
 export default function Home() {
-  console.log(fileTemplate)
   return (
     <div>
       <ShowFilesPage />
--- src/containers/Root.tsx
+++ src/containers/Root.tsx
@@ -1,6 +1,10 @@
-import * as React from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import brown from '@material-ui/core/colors/brown';
+
 import { Store } from '../reducers/types';
 import Routes from '../Routes';
 import { History } from 'history';
@@ -10,12 +14,25 @@ type Props = {
   history: History;
 };
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: red,
+    secondary: brown
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
 export default function Root(props: Props) {
   const { store, history } = props;
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <Routes />
+        <MuiThemeProvider theme={theme}>
+          <Routes />
+        </MuiThemeProvider>
       </ConnectedRouter>
     </Provider>
   );
