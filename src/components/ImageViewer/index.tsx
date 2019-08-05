e81ded1562dee45c6c31b42560196836cc9b4106
Francisco Pizarro <francisco_p@outlook.com>
2019-08-04 23:20:26 -0400

    feat: draw box in real time (#130)

--- src/components/ImageViewer/index.tsx
+++ src/components/ImageViewer/index.tsx
@@ -20,9 +20,15 @@ export interface RectLabel {
   endCoordinates: Coordinate;
 }
 
+interface TempBox {
+  hidden: boolean;
+  coordinates: RectLabel;
+}
+
 interface ImageViewerState {
   startCoordinates: Coordinate;
   endCoordinates: Coordinate;
+  tempBox: TempBox;
 }
 
 interface ImageViewerProps {
@@ -43,16 +49,67 @@ const calculatePosition = (resizeValue: number, originalValue: number) => (
 };
 
 class ImageViewer extends React.Component<ImageViewerProps, ImageViewerState> {
+  state: ImageViewerState = {
+    tempBox: {
+      hidden: true,
+      coordinates: {
+        startCoordinates: { x: 0, y: 0 },
+        endCoordinates: { x: 0, y: 0 }
+      }
+    },
+    endCoordinates: { x: 0, y: 0 },
+    startCoordinates: { x: 0, y: 0 }
+  };
+
   private imageRef = createRef<HTMLImageElement>();
   private containerRef = createRef<HTMLImageElement>();
 
   onPanStart = (event: any, info: any) => {
     this.setState({ startCoordinates: { x: info.point.x, y: info.point.y } });
+
+    if (this.containerRef.current) {
+      const tempBox = {
+        hidden: false,
+        coordinates: {
+          startCoordinates: {
+            x: info.point.x - this.containerRef.current.scrollLeft,
+            y: info.point.y - 106 + this.containerRef.current.scrollTop
+          },
+          endCoordinates: { x: info.point.x, y: info.point.y }
+        }
+      };
+      this.setState({ tempBox });
+    }
+  };
+
+  onPan = (event: any, info: any) => {
+    if (this.containerRef.current) {
+      const tempBox = {
+        ...this.state.tempBox,
+        coordinates: {
+          ...this.state.tempBox.coordinates,
+          endCoordinates: {
+            x: info.point.x - this.containerRef.current.scrollLeft,
+            y: info.point.y - 106 + this.containerRef.current.scrollTop
+          }
+        }
+      };
+      this.setState({ tempBox });
+    }
   };
 
   onPanEnd = (event: any, info: any) => {
     const { insertLabel } = this.props;
     const { startCoordinates } = this.state;
+
+    const tempBox = {
+      hidden: true,
+      coordinates: {
+        startCoordinates: { x: 0, y: 0 },
+        endCoordinates: { x: 0, y: 0 }
+      }
+    };
+    this.setState({ tempBox });
 
     if (this.imageRef.current && this.containerRef.current) {
       const position = calculatePosition(
@@ -90,6 +147,7 @@ class ImageViewer extends React.Component<ImageViewerProps, ImageViewerState> {
 
   render() {
     const { image, labels, labelSelected, availableHeight, classes, onRemoveLabel } = this.props;
+    const nextId = labels.length > 0 ? labels[labels.length - 1].id + 1 : 0;
     let divContainerStyle = {};
     if (this.imageRef.current) {
       divContainerStyle = {
@@ -98,10 +156,16 @@ class ImageViewer extends React.Component<ImageViewerProps, ImageViewerState> {
       };
     }
 
+    const tempLabel = {
+      id: nextId,
+      image: this.state.tempBox.coordinates
+    };
+
     return (
       <Grid container spacing={0} style={{ height: availableHeight }}>
         <div ref={this.containerRef} className={classes.container}>
           <motion.div
+            onPan={this.onPan}
             onPanEnd={this.onPanEnd}
             onPanStart={this.onPanStart}
             style={divContainerStyle}
@@ -115,6 +179,15 @@ class ImageViewer extends React.Component<ImageViewerProps, ImageViewerState> {
                 onRemove={onRemoveLabel}
               />
             ))}
+            {!this.state.tempBox.hidden && (
+              <Label
+                key={'temp.box'}
+                active={true}
+                labelNumber={tempLabel.id}
+                label={tempLabel}
+                onRemove={onRemoveLabel}
+              />
+            )}
             <img
               ref={this.imageRef}
               className={classes.image}
