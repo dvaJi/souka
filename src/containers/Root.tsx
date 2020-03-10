8586135c941d7a60f9dc8ff2457d41238757492b
dvaJi <francisco_p@outlook.com>
2020-03-09 23:05:18 -0300

    chore: remove material ui and implement github ci

--- src/containers/Root.tsx
+++ src/containers/Root.tsx
@@ -4,10 +4,6 @@ import { History } from 'history';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
-import brown from '@material-ui/core/colors/brown';
-
 import Routes from '../Routes';
 
 interface Props {
@@ -15,22 +11,12 @@ interface Props {
   history: History;
 }
 
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-    primary: red,
-    secondary: brown
-  }
-});
-
 export default function Root(props: Props) {
   const { store, history } = props;
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <MuiThemeProvider theme={theme}>
-          <Routes />
-        </MuiThemeProvider>
+        <Routes />
       </ConnectedRouter>
     </Provider>
   );
